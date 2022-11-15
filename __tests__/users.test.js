0be9a4980ec626eda04d2b454a6241e32961d6aa
Julie Nisbet <jmnisbet@gmail.com>
2022-11-15 10:49:30 -0800

    adds tests and boilerplate controller

---
+++ __tests__/users.test.js
@@ -0,0 +1,95 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('users', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  // Dummy user for testing
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+  it('POST /api/v1/users creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser)
+    expect(res.status).toBe(200)
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+
+  it.skip('POST /api/v1/sessions signs in an existing user', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@example.com', password: '12345' });
+    expect(res.status).toEqual(200);
+  });
+
+  it.skip('/protected should return a 401 if not authenticated', async () => {
+    const res = await request(app).get('/api/v1/users/protected');
+    expect(res.status).toEqual(401);
+  });
+
+  it.skip('/protected should return the current user if authenticated', async () => {
+    const agent = request.agent(app);
+    const res = await agent.get('/api/v1/users/protected');
+    expect(res.status).toEqual(200);
+  });
+
+  it.skip('GET api/v1/users should return 401 if user not admin', async () => {
+    const agent = request.agent(app);
+    // create a User directly in the database (saves an API call)
+    const user = await UserService.create({ ...mockUser});
+    // sign in that user
+    await (await agent.post('/api/v1/users/sessions')).send({ email: 'test@example.com', password: '12345' });
+    
+    const res = await agent.get('/api/v1/users/');
+    expect(res.status).toEqual(403);
+  });
+
+  it.skip('/users should return 200 if user is admin', async () => {
+    const agent = request.agent(app);
+
+    // create an admin user
+    await agent.post('/api/v1/users').send({
+      email: 'admin',
+      password: '1234',
+      firstName: 'admin',
+      lastName: 'admin',
+    });
+    // sign in the admin
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: 'admin', password: '1234' });
+
+    // const [agent] = await registerAndLogin({ email: 'admin' });
+    const res = await agent.get('/api/v1/users/');
+    console.log(res.body);
+    expect(res.status).toEqual(200);
+  });
+
+  it.skip('DELETE /sessions deletes the user session', async () => {
+    const agent = request.agent(app);
+    // create a User directly in the database (saves an API call)
+    const user = await UserService.create({ ...mockUser});
+    // sign in that user
+    await (await agent.post('/api/v1/users/sessions')).send({ email: 'test@example.com', password: '12345' });
+    
+    const resp = await agent.delete('/api/v1/users/sessions');
+    expect(resp.status).toBe(204);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
